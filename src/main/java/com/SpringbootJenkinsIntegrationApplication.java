ed31681b82f5618095de4df7041693c1576f2822
chaman bharti <chaman.bharti84@gmail.com>
2021-08-12 14:33:20 +0530

    first commit

---
+++ src/main/java/com/SpringbootJenkinsIntegrationApplication.java
@@ -0,0 +1,31 @@
+package com;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootJenkinsIntegrationApplication {
+
+	Logger log = LoggerFactory.getLogger(SpringbootJenkinsIntegrationApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		log.info("Application started...");
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsIntegrationApplication.class, args);
+	}
+	
+//	/*
+//	 * @GetMapping("/ping") public String message() { return
+//	 * "Wao!! Application Deployed successfully in SAP Cloud.."; }
+//	 */
+
+}
