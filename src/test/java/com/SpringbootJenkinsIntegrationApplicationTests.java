e0484c8dbebe4cd6044105df266ed35ccba0716e
chaman bharti <chaman.bharti84@gmail.com>
2021-08-12 15:02:49 +0530

    fourth commit

--- src/test/java/com/SpringbootJenkinsIntegrationApplicationTests.java
+++ src/test/java/com/SpringbootJenkinsIntegrationApplicationTests.java
@@ -14,7 +14,7 @@ class SpringbootJenkinsIntegrationApplicationTests {
 
 	Logger logger = LoggerFactory.getLogger(SpringbootJenkinsIntegrationApplicationTests.class);
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 		logger.info("Test case executing...");
 		assertEquals(true, true);
 	}
